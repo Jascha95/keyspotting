2366e6e819deb8c8c5f9bb3677914d4b5a445552
ya_dem <yasarkemal.demirelli@uzh.ch>
2026-06-09 14:16:43 +0200

    register added

---
+++ dynap-se1/register.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+echo "Script running"
+
+
+OS=$(uname)
+
+echo $OS "aka aka OSX(mac)"
+if [[ $OS == "Darwin" ]]; then
+    echo "running on mac"
+    VAR=$(ioreg -p IOUSB | grep -i "Dynapse")
+else
+    echo "only $(ioreg -p IOUSB -w0 | grep -E "+-o|USB Product"
+) internals \n $(system_profiler SPUSBDataType) "
+    if [ -n "$VAR" ]; then
+        echo "device found on $OS find at $VAR"
+    fi
+
+
+fi
+
+if [[ $OS != "Darwin" ]]; then
+    echo "$OS"
+    echo "running on linux"
+fi
