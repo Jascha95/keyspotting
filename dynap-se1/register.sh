b3a9b25395ca21329a407a2bef265e964d81cf4e
ya_dem <yasarkemal.demirelli@uzh.ch>
2026-06-12 10:13:09 +0200

    new register

--- dynap-se1/register.sh
+++ dynap-se1/register.sh
@@ -3,6 +3,7 @@
 OS=$(uname)
 
 
+###########################################################_MAC
 if [[ "$OS" == "Darwin" ]]; then
     echo "running on mac"
     VAR=$(ioreg -p IOUSB | grep -i "Dynapse")
@@ -26,25 +27,38 @@ for d in devices:
         print(f'Bus {d.usb_bus_number:03d} Device {d.usb_device_address:03d}')
 ")
 
-OTHER_BOARDS=$(python -c "import samna; devices = samna.device.get_all_devices(); [print(d) for d in devices]" | while IFS= read -r line; do
-    echo -e "  → $line ")
+OTHER_BOARDS=$(python -c "import samna; devices = samna.device.get_all_devices(); [print(d) for d in devices]"| while IFS= read -r line; do
+    echo -e "  → $line " 
+    done)
 
-# if [[ $OS == "Linux" ]]; then
-if [[ "$OS" != "Darwin" ]]; then
+echo "ok === "
+
+
+#
+if [[ "$OS" == "Linux" ]]; then
     echo "$OS"
     echo "devices avail:"
     python -c "import samna; devices = samna.device.get_all_devices(); [print(d) for d in devices]" | while IFS= read -r line; do
     echo -e "  → $line "
     done
-    
+
     echo -e "\n"
     # JAMES=$(lsusb  | grep "Thesycon")
-    if [[-n "$JAMES"]]; then 
-        echo "007 found " 
+    if [[ -n "$JAMES" ]]; then
+        echo "007 found "
         echo -e "James: \n$JAMES"
     else
     echo "James (serial $TARGET_SERIAL) not found $OTHER_BOARDS"
     #lsusb | while read line; do echo "$line"; done
-    
-    fi 
+
+    fi
 fi
+###########################################################_WINDOWS
+
+if [[ "$(hostname)" == "DESKTOP-PC" ]]; then
+    hostname
+    echo "WINDOWS oje"
+fi
+
+
+echo "IF OSError: <<[Errno 13] Permission denied:>> occur, start venv of samna "
